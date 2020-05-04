284489c0b3dcdd8fb5f4e5fc9dd5cbc83de2d5c2
Andreas Kochmann <andreaskochmann85@gmail.com>
2020-05-04 17:35:19 +0200

    added timer canvas + timer script

---
+++ Assets/Scripts/UserInterface/UiTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiTimer : MonoBehaviour
+{
+
+    Text timer;
+    float startTime = 15.0f;
+
+    private void Awake()
+    {
+        timer = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        startTime = startTime - Time.deltaTime;
+        timer.text = startTime.ToString("#.00");
+        Debug.Log(timer.text);
+    }
+}
